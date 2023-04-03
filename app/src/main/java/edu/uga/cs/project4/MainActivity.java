39ae2dc06d7f7bbada21cff32ae3016edc626711
Paul Lim <paulslim47@gmail.com>
2023-04-03 13:22:13 -0400

    trying to add SQL database

--- app/src/main/java/edu/uga/cs/project4/MainActivity.java
+++ app/src/main/java/edu/uga/cs/project4/MainActivity.java
@@ -1,15 +1,34 @@
 package edu.uga.cs.project4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // test
+    public class SqlExperiment extends SQLiteOpenHelper{
+
+        public SqlExperiment(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
+    }
 }
