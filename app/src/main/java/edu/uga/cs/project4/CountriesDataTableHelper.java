7a3d3654c76fd63f7d26d612d8df375bc8e8c8fb
Paul Lim <paulslim47@gmail.com>
2023-04-03 15:58:02 -0400

    Working on adding Countries data into sql datbase

--- app/src/main/java/edu/uga/cs/project4/CountriesDataTableHelper.java
+++ app/src/main/java/edu/uga/cs/project4/CountriesDataTableHelper.java
@@ -1,5 +1,6 @@
 package edu.uga.cs.project4;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,14 +10,18 @@ import androidx.annotation.Nullable;
 public class CountriesDataTableHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "countries.db";
-    private static final int DB_VERSION = 1;
+    private static final int    DB_VERSION = 1;
     private static CountriesDataTableHelper helperInstance;
-    public static final String TABLE_COUNTRIES = "countries";
-    public static final String COUNTRIES_COLUMN_ID = "_id";
-    public static final String COUNTRIES_COLUMN_NAME = "name";
-    public static final String COUNTRIES_COLUMN_CONTINENT = "continent";
-
-
+    public static final String  TABLE_COUNTRIES = "countries";
+    public static final String  COUNTRIES_COLUMN_ID = "_id";
+    public static final String  COUNTRIES_COLUMN_NAME = "name";
+    public static final String  COUNTRIES_COLUMN_CONTINENT = "continent";
+    private static final String CREATE_COUNTRIES =
+        "create table " + TABLE_COUNTRIES + "("
+            + COUNTRIES_COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COUNTRIES_COLUMN_NAME + "TEXT,"
+            + COUNTRIES_COLUMN_CONTINENT + "TEXT,"
+            + ")";
     public CountriesDataTableHelper( Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
@@ -28,12 +33,6 @@ public class CountriesDataTableHelper extends SQLiteOpenHelper {
         return helperInstance;
     }
 
-    private static final String CREATE_COUNTRIES =
-            "create table " + TABLE_COUNTRIES + "("
-            + COUNTRIES_COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COUNTRIES_COLUMN_NAME + "TEXT, "
-            + COUNTRIES_COLUMN_CONTINENT + "TEXT, "
-            + ")";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -44,6 +43,28 @@ public class CountriesDataTableHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL ( "drop table if exists " + TABLE_COUNTRIES );
         onCreate( db );
+    }
+
+    public class CountriesData {
+        private SQLiteDatabase db;
+        private SQLiteOpenHelper countriesDbHelper;
+        public CountriesData( Context context ) {
+            this.countriesDbHelper = CountriesDataTableHelper.getInstance( context );
+        }
+        public void open() {
+            db = countriesDbHelper.getWritableDatabase();
+        }
+        public void close() {
+            if( countriesDbHelper != null )
+                countriesDbHelper.close();
+        }
+    }
+
+    public Countries storeCountry( Countries countries ) {
+        ContentValues values = new ContentValues();
+
+
+
     }
 
 
